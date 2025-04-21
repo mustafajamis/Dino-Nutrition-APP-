52ad8393739b943a78cbd7e63584585427f1eb07
Mustafa Jamshidi <mustafa_jamis1@yahoo.com>
2025-04-20 23:41:20 -0700

    adding the login page

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import WelcomePage from '../screens/Welcome/WelcomePage';
 import OnboardingWrapper from '../screens/OnBoarding/OnboardingWrapper';
+import LoginScreen from '../screens/login/LoginScreen';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ const AppNavigator = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Welcome" component={WelcomePage} />
       <Stack.Screen name="Onboarding" component={OnboardingWrapper} />
+      <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
 };
